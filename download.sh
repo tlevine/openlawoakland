f5cc14093a335d5284034a32b27370b23184040f
Thomas Levine <occurrence@thomaslevine.com>
2013-07-30 10:43:06 -0700

    fix download

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p pdf
-wget -O pdf/volume1.pdf 'https://law.resource.org/pub/us/code/city/Oakland/Oakland%20CA%20Vol%20I%20thru%20Supp%2059.pdf'
-wget -O pdf/volume2.pdf 'https://law.resource.org/pub/us/code/city/Oakland/Oakland%20CA%20Vol%20II%20thru%20Supp%2059.pdf'
-wget -O pdf/planning.pdf 'https://law.resource.org/pub/us/code/city/Oakland/Oakland,%20CA%20Planning%20scanned%20thru%20%2337.pdf'
+wget -O pdf/volume1.pdf 'https://law.resource.org/pub/us/code/city/ca/Oakland/Oakland%20CA%20Vol%20I%20thru%20Supp%2060.pdf'
+wget -O pdf/volume2.pdf 'https://law.resource.org/pub/us/code/city/ca/Oakland/Oakland%20CA%20Vol%20II%20thru%20Supp%2060.pdf'
+wget -O pdf/planning.pdf 'https://law.resource.org/pub/us/code/city/ca/Oakland/Oakland,%20CA%20Planning%20scanned%20thru%20%2337.pdf'
