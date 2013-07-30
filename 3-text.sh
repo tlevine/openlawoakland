153c1fb958906dffe9c7432d6444095053ee255d
Thomas Levine <occurrence@thomaslevine.com>
2013-07-30 12:16:52 -0700

    skip things that are finished or tiny

--- 3-text.sh
+++ 3-text.sh
@@ -3,6 +3,14 @@ set -e
 
 # The jpg and pmm files are just the municode logo.
 for image in $(ls tmp|sed -n '/\.pbm$/ s/\.pbm$//p'); do
-  tesseract "tmp/${image}.pbm" "tmp/${image}" -l eng
-  break
+  if test -e "tmp/${image}.txt"; then
+    echo "Skipping tmp/${image}.pbm because it has already been OCRed"
+    continue
+  elif test $(stat -c %s "tmp/$image.pbm") -lt 10000; then
+    echo "Skipping tmp/${image}.pbm because it's tiny"
+    continue
+  else
+    echo "OCRing tmp/${image}.pbm"
+    tesseract "tmp/${image}.pbm" "tmp/${image}" -l eng
+  fi
 done
