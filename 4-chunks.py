8db9131e6956271b2e82e3213bcd8a60fda96e1e
Thomas Levine <occurrence@thomaslevine.com>
2013-07-31 09:49:11 -0700

    chunker seems to work

--- 4-chunks.py
+++ 4-chunks.py
@@ -6,6 +6,7 @@ section_regex = re.compile(r'^([0-9]+\.[0-9]+\.[0-9]+)(.*)$')
 
 volume = open('tmp/volume1.txt')
 chunks = {}
+section = None
 while True:
     line = volume.readline()
 
@@ -17,6 +18,7 @@ while True:
     if m:
         # Create a new chapter
         section = m.group(1)
+        chunks[section] = m.group(1) + '\n'
         continue
 
     m = re.match(section_regex, line)
@@ -24,7 +26,9 @@ while True:
         # Create a new section
         section = m.group(1)
         chunks[section] = m.group(2) + '\n'
-    else:
+    elif section:
         # Add to the section
-        print section
         chunks[section] += line
+    else:
+        # The first section has yet to start.
+        pass
