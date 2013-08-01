5989a8daf4ec33b8932ca43c449177ea6e4ee516
Thomas Levine <occurrence@thomaslevine.com>
2013-07-31 21:35:38 -0700

    run the chunker on the stuff

--- 4-chunks.py
+++ 4-chunks.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+import os, json
 from lib import chunk
 
-
+for volume in ['volume1', 'volume2', 'planning']:
+    fp_in  = open(os.path.join('tmp', volume + '.txt' ), 'r')
+    fp_out = open(os.path.join('tmp', volume + '.json'), 'w')
+    chunks = chunk(fp_in)
+    json.dump(chunks, fp_out)
