7498da9df372466db956d33b651e412f7846cb64
Thomas Levine <occurrence@thomaslevine.com>
2013-10-09 16:16:40 -0700

    draw_circular

--- 6-plot.py
+++ 6-plot.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 import os
 
-import networkx
+import matplotlib.pyplot as plt
+import networkx as nx
 
-g = networkx.read_adjlist(os.path.join('tmp', 'cross-references.adjlist'), delimiter = ',')
+g = nx.read_adjlist(os.path.join('tmp', 'cross-references.adjlist'), delimiter = ',')
+# colors=range(20)
+# pos=nx.spring_layout(g)
+# nx.draw(g,pos,node_color='#A0CBE2',edge_color=colors,width=4,edge_cmap=plt.cm.Blues,with_labels=False)
+nx.draw_circular(g)
+plt.savefig("edge_colormap.png") # save as png
+plt.show() # display
