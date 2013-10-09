fa959f0f0ed6962ee2e8ab2921453ccf33f4571f
Thomas Levine <occurrence@thomaslevine.com>
2013-10-09 16:21:39 -0700

    plots

--- 6-plot.py
+++ 6-plot.py
@@ -8,6 +8,7 @@ g = nx.read_adjlist(os.path.join('tmp', 'cross-references.adjlist'), delimiter =
 # colors=range(20)
 # pos=nx.spring_layout(g)
 # nx.draw(g,pos,node_color='#A0CBE2',edge_color=colors,width=4,edge_cmap=plt.cm.Blues,with_labels=False)
-nx.draw_circular(g)
-plt.savefig("edge_colormap.png") # save as png
-plt.show() # display
+
+g_9_56_090 = nx.ego_graph(g, '9.56.090')
+nx.draw_spring(g_9_56_090)
+plt.savefig("9.56.090.png")
