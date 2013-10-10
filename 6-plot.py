db925aa1f34711ece32ec2fcc5250f7ff0c4c931
Thomas Levine <occurrence@thomaslevine.com>
2013-10-10 16:31:11 -0700

    index

--- 6-plot.py
+++ 6-plot.py
@@ -10,6 +10,9 @@ g = nx.read_adjlist(os.path.join('tmp', 'cross-references.adjlist'), delimiter =
 nodes = json.load(open(os.path.join('tmp', 'chunks.json')))
 
 def ego():
+    '''
+    This node is deceptively connected because it's from the index (page 750).
+    '''
     g_9_56_090 = nx.ego_graph(g, '9.56.090')
     nx.draw_spring(g_9_56_090)
     plt.savefig("9.56.090.png")
