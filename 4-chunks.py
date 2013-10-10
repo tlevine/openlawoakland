b8792e3c870db78e1b1b48fa4d6abdcd03ee3a9d
Thomas Levine <occurrence@thomaslevine.com>
2013-10-09 17:58:44 -0700

    oeu

--- 4-chunks.py
+++ 4-chunks.py
@@ -8,6 +8,6 @@ for volume in ['volume1', 'volume2', 'planning']:
     fp_in  = open(os.path.join('tmp', volume + '.txt' ), 'r')
     fp_out = open(os.path.join('tmp', volume + '.json'), 'w')
     chunks = chunk(fp_in)
-    all_chunks.extend(chunks)
+    all_chunks.update(chunks)
     json.dump(chunks, fp_out)
 json.dump(all_chunks, fp_out_all)
--- 6-plot.py
+++ 6-plot.py
@@ -5,11 +5,11 @@ import json
 import matplotlib.pyplot as plt
 import networkx as nx
 
-g = nx.read_adjlist(os.path.join('tmp', 'cross-references.adjlist'), delimiter = ',')
-# colors=range(20)
-# pos=nx.spring_layout(g)
-# nx.draw(g,pos,node_color='#A0CBE2',edge_color=colors,width=4,edge_cmap=plt.cm.Blues,with_labels=False)
 
-g_9_56_090 = nx.ego_graph(g, '9.56.090')
-nx.draw_spring(g_9_56_090)
-plt.savefig("9.56.090.png")
+g = nx.read_adjlist(os.path.join('tmp', 'cross-references.adjlist'), delimiter = ',')
+nodes = json.load(open(os.path.join('tmp', 'chunks.json')))
+
+def ego():
+    g_9_56_090 = nx.ego_graph(g, '9.56.090')
+    nx.draw_spring(g_9_56_090)
+    plt.savefig("9.56.090.png")
